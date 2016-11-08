1c5d5b7a839a11c9fcf4195f755c7aad0a631139
Aidar <aidarshaifutdinov@gmail.com>
2016-11-08 17:57:35 +0300

    Added 'fetch from API' functionality. Does not work.

--- app_components/hackathons_list.js
+++ app_components/hackathons_list.js
@@ -13,6 +13,15 @@ export default class HackathonsList extends React.Component {
   constructor(props) {
     super(props);
 
+    // Почему-то с телефона не могу зайти вообще на веб-сервер на localhost`е
+    // fetch('http://172.16.99.1:8000/hack/list')
+    //   .then((response) => {
+    //     hackList = response.json();
+    //     console.log(hackList);
+    //   })
+    //   .catch((error) => console.error(error))
+    //   .done();
+
     // values are hardcoded. Use fetch method to fetch data from API.
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
