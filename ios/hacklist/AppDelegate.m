dcc7950f10ea9df68b55e7f794e18bd79fd46688
Nail Alishev <nailalishev@yahoo.com>
2016-11-24 23:40:52 +0300

    update .gitignore

--- ios/hacklist/AppDelegate.m
+++ ios/hacklist/AppDelegate.m
@@ -18,7 +18,8 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  // jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  jsCodeLocation = [NSURL URLWithString:@"http://192.168.0.3:8081/index.ios.bundle"];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"hacklist"
