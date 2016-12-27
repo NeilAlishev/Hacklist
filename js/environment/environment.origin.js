76c002ef9087af80bf060566750304b0ee0c0fb6
paradisensei <aidarshaifutdinov@gmail.com>
2016-12-27 12:17:38 +0300

    GitHub auth sketch

--- js/environment/environment.origin.js
+++ js/environment/environment.origin.js
@@ -1,6 +1,13 @@
 const Environments = {
   production:  {BASE_URL: '', API_KEY: ''},
-  development: {BASE_URL: '', API_KEY: ''}
+  development: {
+    BASE_URL: '',
+
+    githubClientId: '',
+    githubSecret: '',
+    githubOAuth: 'https://github.com/login/oauth/authorize?client_id=',
+    githubToken: 'https://github.com/login/oauth/access_token'
+  }
 }
 
 export default Environments.development
