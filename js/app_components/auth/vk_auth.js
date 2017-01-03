d1ccc05ce5637288cf7b27f89b702b8ee9a78900
paradisensei <aidarshaifutdinov@gmail.com>
2017-01-03 20:25:55 +0300

    Vk auth at first approximation

---
+++ js/app_components/auth/vk_auth.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  WebView,
+  AsyncStorage
+} from 'react-native';
+
+import Environment from '../../environment/environment';
+
+export default class VkAuthPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      auth: false
+    };
+  }
+
+  render() {
+    return (
+      <WebView
+        source={{uri: Environment.vkOAuth}}
+        onNavigationStateChange={
+          onNavigationStateChangeCallback.bind(this)
+        }
+      />
+    );
+  }
+}
+
+function onNavigationStateChangeCallback(state) {
+  let codePattern = 'code=';
+  let codeIdx = state.url.search(codePattern);
+  if (!this.state.auth && codeIdx != -1) {
+    this.setState({
+      auth: true
+    });
+    let code = state.url.substr(codeIdx + codePattern.length);
+    fetch(Environment.vkToken + code)
+      .then(response => response.json())
+      .then(token => {
+        AsyncStorage.setItem('token', JSON.stringify(token));
+        this.props.navigator.push({
+          id: 'main'
+        });
+      })
+      .catch((error) => console.error(error))
+      .done();
+  }
+}
--- js/environment/environment.origin.js
+++ js/environment/environment.origin.js
@@ -7,6 +7,9 @@ const Environments = {
     githubSecret: '',
     githubOAuth: 'https://github.com/login/oauth/authorize?client_id=',
     githubToken: 'https://github.com/login/oauth/access_token'
+
+    vkOAuth: 'https://oauth.vk.com/authorize?client_id=&redirect_uri=&response_type=code&v=5.60',
+    vkToken: 'https://oauth.vk.com/access_token?client_id=&client_secret=&redirect_uri=&code='
   }
 }
 
