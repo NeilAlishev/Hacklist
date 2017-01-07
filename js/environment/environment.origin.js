d98c80abd24e89a4c027826756bcd6e2ef94d1f0
paradisensei <aidarshaifutdinov@gmail.com>
2017-01-07 12:22:14 +0300

    Client only authenticate gitHub app and saves client_token

--- js/environment/environment.origin.js
+++ js/environment/environment.origin.js
@@ -3,10 +3,7 @@ const Environments = {
   development: {
     BASE_URL: '',
 
-    githubClientId: '',
-    githubSecret: '',
     githubOAuth: 'https://github.com/login/oauth/authorize?client_id=',
-    githubToken: 'https://github.com/login/oauth/access_token'
 
     vkOAuth: 'https://oauth.vk.com/authorize?client_id=&redirect_uri=&response_type=code&v=5.60',
     vkToken: 'https://oauth.vk.com/access_token?client_id=&client_secret=&redirect_uri=&code='
