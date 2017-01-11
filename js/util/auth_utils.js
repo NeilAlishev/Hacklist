d063eded4e39989e664a8bbe383c5e1b05224387
Nail Alishev <nailalishev@yahoo.com>
2017-01-11 12:31:24 +0300

    move uid generation

---
+++ js/util/auth_utils.js
@@ -0,0 +1,11 @@
+const PATTERN = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx';
+
+export default AuthUtils = {
+  generateUID: function() {
+    return PATTERN.replace(/[xy]/g, c => {
+      const r = Math.random()*16|0;
+      const v = c == 'x' ? r : (r&0x3|0x8);
+      return v.toString(16);
+    });
+  }
+}
