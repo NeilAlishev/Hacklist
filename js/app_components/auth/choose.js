9cb1108e960965f99bad5bfdc0ad985241547f74
Nail Alishev <nailalishev@yahoo.com>
2017-01-18 21:02:57 +0300

    social buttons ready

--- js/app_components/auth/choose.js
+++ js/app_components/auth/choose.js
@@ -1,20 +1,12 @@
 import React from 'react';
 import {
   View,
-  TouchableHighlight,
-  StyleSheet,
-  Image,
-  Text
+  StyleSheet
 } from 'react-native';
 
-import Route from '../../enums/route';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import SocialButtons from '../helpers/social_buttons';
 
 export default class ChoosePage extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     let authFailedText = null;
     if (this.props.error) {
@@ -22,45 +14,20 @@ export default class ChoosePage extends React.Component {
     }
     return (
       <View style={styles.container}>
-      {authFailedText}
-      <Icon name="rocket" size={30} color="#900" />
-      <TouchableHighlight onPress={githubAuth.bind(this)} style={styles.logo}>
-        <Image
-          style={styles.logo}
-          source={require('../../resources/images/github.png')}
-        />
-      </TouchableHighlight>
-      <TouchableHighlight onPress={vkAuth.bind(this)} style={styles.logo}>
-        <Image
-          style={styles.logo}
-          source={require('../../resources/images/vk.png')}
-        />
-      </TouchableHighlight>
+        <View style={styles.errorBlock}>
+          {authFailedText}
+        </View>
+        <View style={styles.socialBlock}>
+          <SocialButtons navigator={this.props.navigator}/>
+        </View>
       </View>
     );
   }
-}
-
-function githubAuth() {
-  this.props.navigator.push({
-    id: Route.githubAuth
-  });
-};
-
-function vkAuth() {
-  this.props.navigator.push({
-    id: Route.vkAuth
-  });
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FFF',
-  },
-  logo: {
-    height: 60,
-    width: 60,
-    borderRadius: 5
   }
 });
