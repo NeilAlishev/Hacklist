36cc32ccdb37ddaf96a7b94c6bca118e100ac514
paradisensei <aidarshaifutdinov@gmail.com>
2017-02-23 16:17:05 +0300

    Modify properties

--- js/environment/environment.origin.js
+++ js/environment/environment.origin.js
@@ -1,11 +1,16 @@
 const Environments = {
-  production:  {BASE_URL: '', API_KEY: ''},
-  development: {
-    BASE_URL: '',
+  production: {
+    BASE_URL: 'http://prod_ip_here:8080',
 
     githubOAuth: 'https://github.com/login/oauth/authorize?client_id=&',
-    vkOAuth: 'https://oauth.vk.com/authorize?client_id=&redirect_uri=http://your_ip_here:8080/api/oauth/vk&response_type=code&v=5.62&'
+    vkOAuth: 'https://oauth.vk.com/authorize?client_id=&redirect_uri=BASE_URL/api/oauth/vk&response_type=code&v=5.62&'
+  },
+  development: {
+    BASE_URL: 'http://your_ip_here:8080',
+
+    githubOAuth: 'https://github.com/login/oauth/authorize?client_id=&',
+    vkOAuth: 'https://oauth.vk.com/authorize?client_id=&redirect_uri=BASE_URL/api/oauth/vk&response_type=code&v=5.62&'
   }
 }
 
-export default Environments.development
+export default Environments.{development/production}
