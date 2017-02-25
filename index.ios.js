7f17932a80a5f68fb2c7f2fd014a03a5d72824e1
Neil Alishev <alishev.neil@gmail.com>
2017-02-25 11:09:11 +0300

    enable codepush

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
+import codePush from "react-native-code-push";
 
 import InitialDispatcher from './js/app_components/initial_dispatcher';
+
+const codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME };
 
 export default class Hacklist extends React.Component {
   render() {
@@ -9,4 +12,4 @@ export default class Hacklist extends React.Component {
   }
 }
 
-AppRegistry.registerComponent('Hacklist', () => Hacklist);
+AppRegistry.registerComponent('Hacklist', () => codePush(codePushOptions)(Hacklist));
